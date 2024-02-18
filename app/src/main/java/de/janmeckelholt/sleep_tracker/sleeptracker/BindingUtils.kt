d2dd2d4b2c3235f74a7a82c6982cfa2b7eeca8e3
JMeckel <Jan.Meckelholt@mercedes-benz.com>
2024-02-18 15:38:56 +0100

    introduce binding adapter

---
+++ app/src/main/java/de/janmeckelholt/sleep_tracker/sleeptracker/BindingUtils.kt
@@ -0,0 +1,38 @@
+package de.janmeckelholt.sleep_tracker.sleeptracker
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import de.janmeckelholt.sleep_tracker.R
+import de.janmeckelholt.sleep_tracker.convertDurationToFormatted
+import de.janmeckelholt.sleep_tracker.convertNumericQualityToString
+import de.janmeckelholt.sleep_tracker.database.SleepNight
+
+@BindingAdapter("sleepDurationFormatted")
+fun TextView.setSleepDurationFormatted(item: SleepNight?){
+    item?.let {
+        text = convertDurationToFormatted(it.startTimeMilli, it.endTimeMilli, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityText")
+fun TextView.setSleepQualityText(item: SleepNight?){
+    item?.let {
+        text = convertNumericQualityToString(it.sleepQuality, context.resources)
+    }
+}
+
+@BindingAdapter("sleepQualityImage")
+fun ImageView.setSleepQualityImage(item: SleepNight?){
+    item?.let {
+        setImageResource(when (it.sleepQuality) {
+            0 -> R.drawable.ic_sleep_0
+            1 -> R.drawable.ic_sleep_1
+            2 -> R.drawable.ic_sleep_2
+            3 -> R.drawable.ic_sleep_3
+            4 -> R.drawable.ic_sleep_4
+            5 -> R.drawable.ic_sleep_5
+            else -> R.drawable.ic_sleep_active
+        })
+    }
+}
--- app/src/main/java/de/janmeckelholt/sleep_tracker/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/de/janmeckelholt/sleep_tracker/sleeptracker/SleepNightAdapter.kt
@@ -1,7 +1,6 @@
 package de.janmeckelholt.sleep_tracker.sleeptracker
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
@@ -12,7 +11,7 @@ import de.janmeckelholt.sleep_tracker.R
 import de.janmeckelholt.sleep_tracker.convertDurationToFormatted
 import de.janmeckelholt.sleep_tracker.convertNumericQualityToString
 import de.janmeckelholt.sleep_tracker.database.SleepNight
-import timber.log.Timber
+import de.janmeckelholt.sleep_tracker.databinding.ListItemSleepNightBinding
 
 
 class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
@@ -25,35 +24,21 @@ class SleepNightAdapter: ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(S
         holder.bind(item)
     }
 
-    class ViewHolder private constructor(itemView: View): RecyclerView.ViewHolder(itemView){
-        val sleepLength: TextView = itemView.findViewById(R.id.sleep_length)
-        val sleepQualityText: TextView = itemView.findViewById(R.id.sleep_quality_text)
-        val sleepQualityImage: ImageView = itemView.findViewById(R.id.sleep_quality_image)
+    class ViewHolder private constructor(
+        private val binding: ListItemSleepNightBinding,
+    ): RecyclerView.ViewHolder(binding.root){
 
         companion object {
             fun from(parent: ViewGroup): ViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.list_item_sleep_night, parent, false)
-                return ViewHolder(view)
+                val binding = ListItemSleepNightBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
             }
         }
 
         fun bind(item: SleepNight) {
-            val res = itemView.context.resources
-            sleepLength.text =
-                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            sleepQualityText.text = convertNumericQualityToString(item.sleepQuality, res)
-            sleepQualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
+            binding.sleep = item
+            binding.executePendingBindings()
         }
     }
 
